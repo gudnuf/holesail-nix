b4c1c32c92b5e57b39526173ecbb2bd42f382fa7
gudnuf <gudnuf21@proton.me>
2024-09-19 13:53:55 -0700

    add flake

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# holesail-nix
-Nixify holesail.io
+# Holesail Nix Flake
+
+This repository contains a Nix flake for building [holesail](https://holesail.io), a P2P based node package to expose your local ports on the Holepunch protocol.
+
+## Usage
+
+To use this flake, make sure you have Nix installed and flakes enabled. Then, you can run:
+
+```bash
+nix build github:gudnuf/holesail-nix
+```
+
+This will build the `holesail` package using the flake.
+
+You can also add this flake as an input to your own flake:
+
+```nix
+inputs.holesail.url = "github:yourusername/holesail-flake";
+```
+
+And then reference the package in your `outputs`:
+
+```nix
+packages.default = inputs.holesail.packages.${system}.default;
+```
---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "A P2P based node package to expose your local ports on the Holepunch protocol";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = rec {
+          holesail = pkgs.buildNpmPackage rec {
+            pname = "holesail";
+            version = "1.7.3";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "gudnuf";
+              repo = pname;
+              rev = "v${version}";
+              hash = "sha256-u7u+bljBEkbLWRrXMDZdgAe1xozx2rlqgnXSFxGqFVk=";
+            };
+
+            npmDepsHash = "sha256-lZEpP14sN62LOv85VsGEIWAHXQuRt6lfhbp/iGpffX4=";
+
+            npmPackFlags = [ "--ignore-scripts" ];
+
+            dontNpmBuild = true;
+            nodejs = pkgs.nodejs_22;
+
+            meta = with pkgs.lib; {
+              description = "Holesail let's you instantly share any application running on a specific port from your local computer.";
+              homepage = "https://holesail.io";
+              license = licenses.mit;
+              maintainers = with maintainers; [ supersuryaansh ];
+            };
+          };
+
+          default = holesail;
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            self.packages.${system}.default
+          ];
+        };
+      }
+    );
+}
